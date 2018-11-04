de4159d0fe103114147e9232fffc79329640c70a
nkerr93 <43756430+nkerr93@users.noreply.github.com>
2018-11-04 15:30:08 -0800

    test

--- Lab3/Leaflet_Example-master/maps/leaflet.js
+++ Lab3/Leaflet_Example-master/maps/leaflet.js
@@ -11,19 +11,17 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   subdomains: ['a', 'b', 'c']
 }).addTo( map )
 
-var myURL = jQuery( 'script[src$="countries.js"]' ).attr( 'src' ).replace( 'countries.js', '' )
-
+ 
 var myIcon = L.icon({
-  iconUrl: myURL + 'images/pin24.png',
-  iconRetinaUrl: myURL + 'images/pin48.png',
-  iconSize: [29, 24],
-  iconAnchor: [9, 21],
-  popupAnchor: [0, -14]
-})
+    iconUrl: 'maps/images/red.png',
+     iconSize: [29, 24],
+    iconAnchor: [9, 21],
+    popupAnchor: [0, -14]
+});
 
 for ( var i=0; i < markers.length; ++i )
 {
- L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+ L.markers( [markers[i].lat, markers[i].lng], {icon: myIcon} )
   .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
   .addTo( map );
 }
