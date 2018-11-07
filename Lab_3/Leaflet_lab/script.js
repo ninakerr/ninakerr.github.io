dcbb73dbbca4fff1464f5e5f8e067b86904d9d35
nkerr93 <43756430+nkerr93@users.noreply.github.com>
2018-11-07 12:45:03 -0800

    lab 3 editing

--- Lab_3/Leaflet_lab/script.js
+++ Lab_3/Leaflet_lab/script.js
@@ -1,39 +1,47 @@
 var map = L.map('map', {
-  'center': [0, 0],
-  'zoom': 0,
+  'center': [26.6, -81.5],
+  'zoom': 6,
   'layers': [
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      'attribution': 'Map data &copy; OpenStreetMap contributors'
+      'attribution': 'Map data &copy; OpenStreetMap contributors',
+    
     })
   ]
 });
 
 var controlLayers = L.control.layers().addTo(map);
 
-var url = 'https://rawgit.com/johan/world.geo.json/master/countries.geo.json';
-
-$.getJSON(url, function (geojson) {
-  var geojsonLayer = L.geoJson(geojson, {
-    style: function (feature) {
-      return {
-        'weight': 0,
-        'fillColor': 'yellow',
-        'fillOpacity': 1
-      }
-    }
-  }).addTo(map);
-  controlLayers.addOverlay(geojsonLayer, 'GeoJSON layer 1');
-});
+var url = 'https://opendata.arcgis.com/datasets/f398a70bf5484c83ae072ac40d5910c9_10.geojson';
 
 $.getJSON(url, function (geojson) {
   var geojsonLayer = L.geoJson(geojson, {
     style: function (feature) {
       return {
         'weight': 1,
-        'color': 'red',
-        'fillOpacity': 0
+        'fillColor': 'grey',
+          'color': 'red',
+        'fillOpacity': 1
       }
     }
   }).addTo(map);
-  controlLayers.addOverlay(geojsonLayer, 'GeoJSON layer 2');
+  controlLayers.addOverlay(geojsonLayer, 'Congressional District');
+});
+var url_b = 'https://opendata.arcgis.com/datasets/9c0667c589de45db9e2992945e7bb491_11.geojson';
+
+$.getJSON(url_b, function (geojson) {
+  var geojsonLayer = L.geoJson(geojson, {
+    style: function (feature) {
+      return {
+        'weight': 1,
+        'fillColor': 'grey',
+        'color': 'blue',
+        'fillOpacity': 1
+      }
+    }
+  }).addTo(map);
+  controlLayers.addOverlay(geojsonLayer, 'Senate Districts');
+  var marker = L.marker([25.4, -81.1]).addTo(map);
+  marker.bindPopup("<b> New House 26 Reresentative:</b><br>Debbie Mucarsel-Powell (D)").openPopup();
+  var marker2 = L.marker([25.69, -80.30]).addTo(map);
+  marker2.bindPopup("<b> New House 27 Reresentative:</b> <br>Donna Shalala(D)").openPopup();
 });
